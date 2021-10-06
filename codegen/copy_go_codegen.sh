576524e5c1701d1928ebec4187e90723994c88a6
Sean McGrail <mcgrails@amazon.com>
2021-10-06 15:35:30 -0700

    Update copy_go_codegen.sh

--- codegen/copy_go_codegen.sh
+++ codegen/copy_go_codegen.sh
@@ -7,5 +7,4 @@ CODGEN_ROOT=$2
 
 REPOTOOLS_VERSION="${REPOTOOLS_VERSION:-latest}"
 
-cd "$1"/internal/repotools
 go run github.com/awslabs/aws-go-multi-module-repository-tools/cmd/gomodgen@${REPOTOOLS_VERSION} -build "$CODGEN_ROOT"
